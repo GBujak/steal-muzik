decbec7dd5cf494020bd4995d15f51e501a6db94
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2022-11-12 14:45:37 +0100

    setup tailwind

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  trailingComma: "es5",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
